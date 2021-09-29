dafb1df4beba3af24e2a8df0d455c7df67bda9ce
juandavido92 <juandavido92@hotmail.com>
2021-09-29 08:06:43 -0500

    subiendo archivo de validaciones hasta el sprint2, modificado ligeramente registro para poder tambien validar desde la interfaz web

---
+++ validaciones.js
@@ -0,0 +1,52 @@
+// Sintaxis del modo estricto para todo el script
+'use strict';
+
+function validar_nombre_usuario(string){
+    if(string.length >= 6 && string.length <= 30){
+        if(string.match(/[a-z]|[A-Z]/g).length != string.length){
+            return false;
+        }
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validar_contrasena(string){
+    if(string.length >= 6){
+        if(string.match(/[a-z]|[A-Z]|[0-9]/g).length != string.length){
+            return false;
+        }
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function confirmar_contrasena(stringA,stringB){
+    if(validar_contrasena(stringA) && validar_contrasena(stringB)){
+        if (stringA === stringB) {
+            return true;
+        } else {
+            return false;
+        }
+    }else{
+        return false;
+    }
+}
+
+module.exports.validar_nombre_usuario = validar_nombre_usuario;
+module.exports.validar_contrasena = validar_contrasena;
+module.exports.confirmar_contrasena = confirmar_contrasena;
+
+function validar_todo(nombre,contrasena1,contrasena2){
+    if(!validar_nombre_usuario(nombre)){
+        alert('Error en nombre de usuario');
+    }
+    else if(!confirmar_contrasena(contrasena1,contrasena2)){
+        alert('Error en contraseñas');
+    }
+    else{
+        alert('Todo OK');
+    }
+}
