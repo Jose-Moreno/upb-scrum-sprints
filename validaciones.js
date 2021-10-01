b1d83bbb859dfca690610973b035b17181264cac
juandavido92 <juandavido92@hotmail.com>
2021-10-01 14:31:56 -0500

    sprint 3 ajustando a lo que pide codegrade

--- validaciones.js
+++ validaciones.js
@@ -1,6 +1,8 @@
 // Sintaxis del modo estricto para todo el script
 'use strict';
 
+let registros = [];
+
 function validar_nombre_usuario(string){
     if(string.length >= 6 && string.length <= 30){
         if(string.match(/[a-z]|[A-Z]/g).length != string.length){
@@ -42,11 +44,40 @@ module.exports.confirmar_contrasena = confirmar_contrasena;
 function validar_todo(nombre,contrasena1,contrasena2){
     if(!validar_nombre_usuario(nombre)){
         alert('Error en nombre de usuario');
+        return false;
     }
     else if(!confirmar_contrasena(contrasena1,contrasena2)){
         alert('Error en contraseñas');
+        return false;
     }
     else{
         alert('Todo OK');
+        return true;
     }
 }
+
+function agregarRegistro(){
+    var usuario = document.getElementById('in_usuario').value;
+    var contrasena = document.getElementById('in_contrasena').value;
+    var confirmar_contrasena = document.getElementById('in_confirmar_contrasena').value;
+    var miObjeto = {'usuario':usuario,'contrasena':contrasena,'confirmar_contrasena':confirmar_contrasena};
+    registros.push(miObjeto);
+}
+
+function OrdenarArreglo(arreglo){
+    arreglo.sort(function(a, b) {
+        if (a.usuario > b.usuario) {
+          return 1;
+        }
+        if (a.usuario < b.usuario) {
+          return -1;
+        }
+        return 0;
+      });
+      console.log(arreglo);
+      return arreglo;
+}
+
+module.exports.registros = registros;
+module.exports.agregarRegistro = agregarRegistro;
+module.exports.OrdenarArreglo = OrdenarArreglo;
